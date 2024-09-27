738a270b8293c3fed787e6d745e768b3517ecc86
Vasyl <vasialama@gmail.com>
2024-09-27 17:24:39 +0300

    tape fix

--- terraform_project/main.tf
+++ terraform_project/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "1.9.6"
   required_providers {
     aws = {
-        source = "hasicorp/aws"
+        source = "hashicorp/aws"
         version = "5.69.0"
     }
   }
