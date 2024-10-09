f4ff1fb478a27ffee217ea1f0f42d8baecd4d171
Vasyl <vasialama@gmail.com>
2024-10-09 14:23:35 +0300

    Commented OIDC part, to create stable connection manually.

--- task_1_dir/iam_roles.tf
+++ task_1_dir/iam_roles.tf
@@ -1,50 +1,52 @@
-resource "aws_iam_openid_connect_provider" "GitHub" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
-}
+#This section commented on establishing a stable, accidently undestroyable connection with Github Actions.
 
-resource "aws_iam_role" "GitHub_Actions" {
-  name               = "GitHub_Actions"
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Federated": "arn:aws:iam::207567795251:oidc-provider/token.actions.githubusercontent.com"
-            },
-            "Action": "sts:AssumeRoleWithWebIdentity",
-            "Condition": {
-                "StringEquals": {
-                    "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
-                },
-                "StringLike": {
-                    "token.actions.githubusercontent.com:sub": "repo:VasylKhytrych/DevOps_course_Vasylkh_2024:*"
-                }
-            }
-        }
-    ]
-}  
-EOF
-}
+# resource "aws_iam_openid_connect_provider" "GitHub" {
+#   url             = "https://token.actions.githubusercontent.com"
+#   client_id_list  = ["sts.amazonaws.com"]
+#   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
+# }
 
-locals {
-  policies = [
-    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
-    "arn:aws:iam::aws:policy/AmazonRoute53FullAccess",
-    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
-    "arn:aws:iam::aws:policy/IAMFullAccess",
-    "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
-    "arn:aws:iam::aws:policy/AmazonSQSFullAccess",
-    "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess",
-    "arn:aws:iam::207567795251:policy/DynamoDB_TFLocks"
-  ]
-}
+# resource "aws_iam_role" "GitHub_Actions" {
+#   name               = "GitHub_Actions"
+#   assume_role_policy = <<EOF
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Effect": "Allow",
+#             "Principal": {
+#                 "Federated": "arn:aws:iam::207567795251:oidc-provider/token.actions.githubusercontent.com"
+#             },
+#             "Action": "sts:AssumeRoleWithWebIdentity",
+#             "Condition": {
+#                 "StringEquals": {
+#                     "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+#                 },
+#                 "StringLike": {
+#                     "token.actions.githubusercontent.com:sub": "repo:VasylKhytrych/DevOps_course_Vasylkh_2024:*"
+#                 }
+#             }
+#         }
+#     ]
+# }  
+# EOF
+# }
 
-resource "aws_iam_role_policy_attachment" "github_actions_policies" {
-  for_each   = toset(local.policies)
-  role       = aws_iam_role.GitHub_Actions.name
-  policy_arn = each.key
-}
+# locals {
+#   policies = [
+#     "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
+#     "arn:aws:iam::aws:policy/AmazonRoute53FullAccess",
+#     "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+#     "arn:aws:iam::aws:policy/IAMFullAccess",
+#     "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
+#     "arn:aws:iam::aws:policy/AmazonSQSFullAccess",
+#     "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess",
+#     "arn:aws:iam::207567795251:policy/DynamoDB_TFLocks"
+#   ]
+# }
+
+# resource "aws_iam_role_policy_attachment" "github_actions_policies" {
+#   for_each   = toset(local.policies)
+#   role       = aws_iam_role.GitHub_Actions.name
+#   policy_arn = each.key
+# }
