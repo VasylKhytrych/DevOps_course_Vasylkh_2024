a47ab0c8fd03dcdf97989e9fc5a511f28db1e326
Vasyl <vasialama@gmail.com>
2024-11-03 13:27:24 +0200

    Changes Jenkins availability config

--- task_4_dir/modules/ec2/user_data_sh/k3s_master.sh
+++ task_4_dir/modules/ec2/user_data_sh/k3s_master.sh
@@ -1,27 +1,60 @@
 #!/bin/bash
-# Install k3s master node
-curl -sfL https://get.k3s.io | K3S_TOKEN=MyFunnyToken12345  sh -s - server --kube-apiserver-arg "bind-address=0.0.0.0" #install k3s
-# Install Helm
+exec > /tmp/user_data_logs.log 2>&1
 
+# Function to log messages with a timestamp
+log() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+# Install k3s
+echo "Installing k3s..."
+curl -sfL https://get.k3s.io | K3S_TOKEN=MyFunnyToken12345 sh -s - server --kube-apiserver-arg "bind-address=0.0.0.0"
+
+# Install Helm
+echo "Installing Helm..."
 curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# Install Git
+echo "Installing Git..."
 sudo yum install git -y
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
+# Set KUBECONFIG variable
+echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> ~/.bashrc
+source ~/.bashrc
+
+# Download configuration files
+wget -P /opt/Jenkins/conf https://raw.githubusercontent.com/VasylKhytrych/DevOps_course_Vasylkh_2024/19df5492a5df540c79fd688722bdee0a796e0125/task_4_dir/jenkins_config/jenkins-volume.yaml
+wget -P /opt/Jenkins/conf https://raw.githubusercontent.com/VasylKhytrych/DevOps_course_Vasylkh_2024/19df5492a5df540c79fd688722bdee0a796e0125/task_4_dir/jenkins_config/jenkins-sa.yaml
+wget -P /opt/Jenkins/conf https://raw.githubusercontent.com/VasylKhytrych/DevOps_course_Vasylkh_2024/19df5492a5df540c79fd688722bdee0a796e0125/task_4_dir/jenkins_config/jenkins-values.yaml
+
+# Create the Jenkins namespace and apply configurations
+cd /opt/Jenkins/conf
+kubectl create namespace jenkins
+kubectl apply -f jenkins-volume.yaml -n jenkins
+kubectl apply -f jenkins-sa.yaml -n jenkins
+
 # Install Jenkins using Helm
-helm repo add jenkins https://charts.jenkins.io
+echo "Setting up Jenkins..."
 helm repo add jenkinsci https://charts.jenkins.io
 helm repo update
-helm search repo jenkinsci
-git clone https://github.com/VasylKhytrych/Helm-Jenkins.git /opt/Jenkins/conf
-cd /opt/Jenkins/conf
 
-kubectl apply -f jenkins-volume.yaml
-kubectl apply -f jenkins-sa.yaml
+# Install Jenkins via Helm
 chart=jenkinsci/jenkins
 helm install jenkins -n jenkins -f jenkins-values.yaml $chart
-# Create the devops-tools namespace
-#kubectl create namespace jenkins
-#helm install jenkins jenkins/jenkins -f jenkins-values.yaml --namespace jenkins
-#kubectl apply -f jenkins-volume.yaml
-#kubectl apply -f jenkins-sa.yaml
 
-#helm install jenkins jenkins/jenkins -f jenkins-values.yaml -n jenkins
+# Wait for 30 seconds
+log "Waiting for 30 seconds before changing ownership of Jenkins volume..."
+sleep 30
+
+# Change ownership of the Jenkins volume
+echo "Changing ownership of Jenkins volume..."
+sudo chown -R 1000:1000 /data/jenkins-volume
+
+# Retrieve the Jenkins admin password and save to a file
+echo "Retrieving Jenkins admin password..."
+jsonpath="{.data.jenkins-admin-password}"
+secret=$(kubectl get secret -n jenkins jenkins -o jsonpath="$jsonpath")
+echo "$secret" | base64 --decode > /tmp/admin_jn_pass.txt
+echo "Jenkins admin password saved to /tmp/admin_jn_pass.txt"
+
+echo "Jenkins setup completed."
