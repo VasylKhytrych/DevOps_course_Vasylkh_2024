27f3b9c9d84a3f716af84a920763129e9ae64a2e
Vasyl <vasialama@gmail.com>
2024-11-23 19:16:46 +0200

    Kick-start of task 6

---
+++ task_6_dir/modules/ec2/user_data_sh/k3s_master.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+exec > /tmp/user_data_logs.log 2>&1
+
+# Function to log messages with a timestamp
+log() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+# Install k3s
+echo "Installing k3s..."
+curl -sfL https://get.k3s.io | K3S_TOKEN=MyFunnyToken12345 sh -s - server --kube-apiserver-arg "bind-address=0.0.0.0"
+
+# Install Helm
+echo "Installing Helm..."
+curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# Install Git
+echo "Installing Git..."
+sudo yum install git -y
+
+# Set KUBECONFIG variable
+echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> ~/.bashrc
+source ~/.bashrc
+
+# Download configuration files
+wget -P /opt/Jenkins/conf https://raw.githubusercontent.com/VasylKhytrych/DevOps_course_Vasylkh_2024/refs/heads/task_6_vasylk/task_6_dir/jenkins_config/jenkins-volume.yaml
+wget -P /opt/Jenkins/conf https://raw.githubusercontent.com/VasylKhytrych/DevOps_course_Vasylkh_2024/refs/heads/task_6_vasylk/task_6_dir/jenkins_config/jenkins-sa.yaml
+wget -P /opt/Jenkins/conf https://raw.githubusercontent.com/VasylKhytrych/DevOps_course_Vasylkh_2024/refs/heads/task_6_vasylk/task_6_dir/jenkins_config/jenkins-values.yaml
+wget -P /opt/Jenkins/conf https://raw.githubusercontent.com/VasylKhytrych/DevOps_course_Vasylkh_2024/refs/heads/task_6_vasylk/task_6_dir/jenkins_config/hello_world_job.xml
+wget -P /opt/Jenkins/conf https://raw.githubusercontent.com/VasylKhytrych/DevOps_course_Vasylkh_2024/refs/heads/task_6_vasylk/task_6_dir/jenkins_config/job_build_start.sh
+
+chmod 777 /opt/Jenkins/conf/job_build_start.sh
+ln -s /opt/Jenkins/conf /root/conf
+
+# Create the Jenkins namespace and apply configurations
+cd /opt/Jenkins/conf
+kubectl create namespace jenkins
+kubectl apply -f jenkins-volume.yaml -n jenkins
+kubectl apply -f jenkins-sa.yaml -n jenkins
+
+# Install Jenkins using Helm
+echo "Setting up Jenkins..."
+helm repo add jenkinsci https://charts.jenkins.io
+helm repo update
+
+# Install Jenkins via Helm
+chart=jenkinsci/jenkins
+helm install jenkins -n jenkins -f jenkins-values.yaml $chart
+
+# Wait for 30 seconds
+log "Waiting for 30 seconds before changing ownership of Jenkins volume..."
+sleep 30
+
+# Change ownership of the Jenkins volume
+echo "Changing ownership of Jenkins volume..."
+#mkdir /data/jenkins-volume
+sudo chown -R 1000:1000 /data/jenkins-volume
+
+# Retrieve the Jenkins admin password and save to a file
+echo "Retrieving Jenkins admin password..."
+jsonpath="{.data.jenkins-admin-password}"
+secret=$(kubectl get secret -n jenkins jenkins -o jsonpath="$jsonpath")
+echo "$secret" | base64 --decode > /root/conf/admin_jn_pass.txt
+echo "Jenkins admin password saved to /root/conf/admin_jn_pass.txt"
+
+echo "Jenkins setup completed."
+
+# Install of GH cli for wp deploys
+# Download and install the GitHub CLI on Amazon Linux 2
+
+
+# echo "Installing Helm..."
+# sudo yum install -y yum-utils
+# sudo yum-config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+# sudo yum install -y gh
+
+# export GH_TOKEN="your token"
+# echo "$GH_TOKEN" | gh auth login --with-token
+
+# # Generate kubeconfig for k3s
+# sudo k3s kubectl config view --raw > kubeconfig
+
+# # Update the GitHub Actions secret with the new kubeconfig
+# gh secret set KUBECONFIG -b"$(< kubeconfig)" -R VasylKhytrych/Helm-Jenkins-WP
+
+# WP install on initialization
+# mkdir -p /opt/wp/
+# ln -s /opt/wp/ /root/wp_conf
+# git clone https://github.com/VasylKhytrych/Helm-Jenkins-WP /opt/wp/
+# cd /opt/wp
+# helm install wordpress ./wordpress-chart/ --namespace default
